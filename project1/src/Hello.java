06f5200b6279e19d9c9209dd4f60093b5d421d60
19jy0105 <19jy0105@PC20.jynet2.jec.ac.jp>
2020-10-22 12:09:55 +0900

    add 時よ止まれ

--- project1/src/Hello.java
+++ project1/src/Hello.java
@@ -3,7 +3,8 @@ public class Hello {
 
 	public static void main(String[] args) {
 		// TODO 自動生成されたメソッド・スタブ
-			System.out.print("za  world");
+			System.out.println("za  world");
+			System.out.println("時よ止まれ");
 	}
 
 }
