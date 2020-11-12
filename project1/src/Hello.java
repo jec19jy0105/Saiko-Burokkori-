2b504e8f4137392a0ca550cbbeb7eff877d67f5b
19jy0105 <19jy0105@PC20.jynet2.jec.ac.jp>
2020-11-12 11:26:46 +0900

    無駄無駄

--- project1/src/Hello.java
+++ project1/src/Hello.java
@@ -4,7 +4,7 @@ public class Hello {
 	public static void main(String[] args) {
 		// TODO 自動生成されたメソッド・スタブ
 			System.out.print("za  world");
-			System.out.print("無駄無駄無駄無駄");
+			System.out.print("無駄無駄無駄");
 	}
 
 }
